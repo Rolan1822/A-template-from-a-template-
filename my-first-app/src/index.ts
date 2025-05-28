e534ad796af2cbeaf9b17e83c4a90fa20291a1f2
Rolan1822 <steveswab@startmail.com>
2025-05-28 17:22:52 +0000

    Add more things

---
+++ my-first-app/src/index.ts
@@ -0,0 +1,18 @@
+import { Probot } from "probot";
+
+export default (app: Probot) => {
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    await context.octokit.issues.createComment(issueComment);
+  });
+  app.onAny(async (context) => {
+    app.log.info({ event: context.name, action: context.payload.action });
+  });
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+};
